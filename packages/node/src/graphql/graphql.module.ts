08a3ed7d93612caf87a7cdb3514abe24ffc631fe
navigaid <navigaid@gmail.com>
2021-06-02 18:00:22 +0800

    clear unused imports

--- packages/node/src/graphql/graphql.module.ts
+++ packages/node/src/graphql/graphql.module.ts
@@ -1,19 +1,9 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import { Module, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { HttpAdapterHost } from '@nestjs/core';
-import { ApolloServer } from 'apollo-server-express';
-import ExpressPinoLogger from 'express-pino-logger';
-import { Pool } from 'pg';
-import { getPostGraphileBuilder } from 'postgraphile-core';
-import { QueryConfig } from '../configure/QueryConfig';
-import { getLogger } from '../utils/logger';
-import { plugins } from './plugins';
+import { Module } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { ApolloService } from './apollo.service';
-import { useSofa } from 'sofa-api';
-import express from 'express';
 
 @Module({
   providers: [ProjectService, ApolloService],
