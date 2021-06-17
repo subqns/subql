623bd65b716ec5db2b1abecb1f8e69d469e6f6af
navigaid <navigaid@gmail.com>
2021-06-17 10:59:55 +0800

    make it possible to run node and query seprately via env var APP

--- packages/node/src/main.ts
+++ packages/node/src/main.ts
@@ -3,17 +3,43 @@
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppNodeModule } from './app.node.module';
+import { AppQueryModule } from './app.query.module';
 import { IndexerManager } from './indexer/indexer.manager';
 import { getLogger, NestLogger } from './utils/logger';
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
+const APP = process.env.APP || "all";
 
-async function bootstrap() {
+async function bootstrapNode() {
+  const app = await NestFactory.create(AppNodeModule);
+  await app.get(IndexerManager).init();
+  await app.init();
+  getLogger('subql-node').info(`subql-node started`);
+}
+
+async function bootstrapQuery() {
+  const app = await NestFactory.create(AppQueryModule);
+  await app.init();
+  await app.listen(PORT);
+  getLogger('subql-query').info(`subql-query started on ${PORT}`);
+}
+
+async function bootstrapAll() {
   const app = await NestFactory.create(AppModule);
   await app.get(IndexerManager).init();
   await app.init();
   await app.listen(PORT);
-  getLogger('subql-node').info(`node started on ${PORT}`);
+  getLogger('subql').info(`subql started on ${PORT}`);
 }
 
-void bootstrap();
+switch (APP) {
+  case 'node':
+    void bootstrapNode();
+    break;
+  case 'query':
+    void bootstrapQuery();
+    break;
+  default:
+    void bootstrapAll();
+}
