4b4af3c594da16114846f04beadfcecbb3349829
navigaid <navigaid@gmail.com>
2021-09-10 10:15:36 +0800

    remove .entity suffix for sequelize entities

--- packages/node/src/entities/index.ts
+++ packages/node/src/entities/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2020-2021 OnFinality Limited authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-export * from './Subquery.entity';
+export * from './Subquery';
 // export * from './Cat.entity';
