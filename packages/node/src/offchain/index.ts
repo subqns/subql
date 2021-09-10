e449c1a9e364045520e85248a19bbd62ea24b971
navigaid <navigaid@gmail.com>
2021-09-10 10:30:27 +0800

    add offchain modules index

---
+++ packages/node/src/offchain/index.ts
@@ -0,0 +1,13 @@
+import { BannerModule } from './Banner.module';
+import { NftViewModule } from './NftView.module';
+import { AccountModule } from './Account.module';
+import { CatModule } from './Cat.module';
+import { HealthModule } from './Health.module';
+
+export const OffchainModules = [
+  BannerModule,
+  NftViewModule,
+  AccountModule,
+  CatModule,
+  HealthModule,
+];
