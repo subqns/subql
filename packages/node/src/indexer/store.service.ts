5e449d080a680c54e3f9551949afd587bc0293d0
navigaid <navigaid@gmail.com>
2021-10-12 20:57:54 +0800

    no insert

--- packages/node/src/indexer/store.service.ts
+++ packages/node/src/indexer/store.service.ts
@@ -62,6 +62,7 @@ export class StoreService {
   async syncoffchain(dbSchema: string): Promise<void> {
     let offchainSchema = process.env.DB_SCHEMA ?? 'public';
     let extraQueries = [
+      /*
       // populate data, note that updated_at isn't updated unless row is changed via typeorm
       // can also be done in Account.service's onModuleInit stage
       `INSERT INTO ${offchainSchema}.offchain_account (id, name) VALUES
@@ -72,23 +73,24 @@ export class StoreService {
       `CREATE OR REPLACE FUNCTION ${dbSchema}.accounts_name(acc ${dbSchema}.accounts) RETURNS text AS $$
         SELECT name FROM ${offchainSchema}.offchain_account WHERE id = acc.id
         $$ LANGUAGE sql STABLE;`,
+       */
     ];
 
     if (false) {
       extraQueries = extraQueries.concat([
-	`DROP VIEW IF EXISTS public.orders`,
-	`DROP VIEW IF EXISTS public.order_items`,
-	`DROP VIEW IF EXISTS public.classes`,
-	`DROP VIEW IF EXISTS public.categories`,
-	`DROP VIEW IF EXISTS public.nfts`,
-	`DROP VIEW IF EXISTS public.accounts`,
-	`CREATE OR REPLACE VIEW public.orders AS SELECT * FROM ${dbSchema}.orders`,
-	`CREATE OR REPLACE VIEW public.order_items AS SELECT * FROM ${dbSchema}.order_items`,
-	`CREATE OR REPLACE VIEW public.classes AS SELECT * FROM ${dbSchema}.classes`,
-	`CREATE OR REPLACE VIEW public.categories AS SELECT * FROM ${dbSchema}.categories`,
-	`CREATE OR REPLACE VIEW public.nfts AS SELECT * FROM ${dbSchema}.nfts`,
-	`CREATE OR REPLACE VIEW public.accounts AS SELECT * FROM ${dbSchema}.accounts`,
-      ])
+        `DROP VIEW IF EXISTS public.orders`,
+        `DROP VIEW IF EXISTS public.order_items`,
+        `DROP VIEW IF EXISTS public.classes`,
+        `DROP VIEW IF EXISTS public.categories`,
+        `DROP VIEW IF EXISTS public.nfts`,
+        `DROP VIEW IF EXISTS public.accounts`,
+        `CREATE OR REPLACE VIEW public.orders AS SELECT * FROM ${dbSchema}.orders`,
+        `CREATE OR REPLACE VIEW public.order_items AS SELECT * FROM ${dbSchema}.order_items`,
+        `CREATE OR REPLACE VIEW public.classes AS SELECT * FROM ${dbSchema}.classes`,
+        `CREATE OR REPLACE VIEW public.categories AS SELECT * FROM ${dbSchema}.categories`,
+        `CREATE OR REPLACE VIEW public.nfts AS SELECT * FROM ${dbSchema}.nfts`,
+        `CREATE OR REPLACE VIEW public.accounts AS SELECT * FROM ${dbSchema}.accounts`,
+      ]);
     }
 
     for (const query of extraQueries) {
